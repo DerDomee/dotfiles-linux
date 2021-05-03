6f12b50de8b1f6e51fad6a322946e105f05fcd77
Dominik Riedig <me@dominikriedig.de>
2021-05-03 15:28:46 +0200

    Add more workspaces (20) and setup for multiple polybar bars

--- .config/polybar/launch_singlemonitor.sh
+++ .config/polybar/launch_singlemonitor.sh
@@ -5,6 +5,6 @@ killall -q polybar
 # Wait for old polybar processes to terminate correctly
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Start new polybar instances
-polybar topbar --config=$HOME/.config/polybar/config 2>&1 | tee -a /tmp/polybar.log & disown;
+polybar singlemonitor --config=$HOME/.config/polybar/config 2>&1 | tee -a /tmp/polybar.log & disown;
 
 echo "Bars launched"
