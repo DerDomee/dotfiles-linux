2b03fec67eede8739be2d2c0a1e85f53b43f359a
Dominik Riedig <me@dominikriedig.de>
2021-05-08 15:47:58 +0200

    Automatically paste selected emoji in current window

--- .local/bin/dmenu_emoji_picker.sh
+++ .local/bin/dmenu_emoji_picker.sh
@@ -1,1 +1,1 @@
-cat ~/.local/bin/emoji_list.txt | dmenu $@ | awk '{print $1;}' | tr -d '\n' | xclip -selection clipboard
+cat ~/.local/bin/emoji_list.txt | dmenu $@ | awk '{print $1;}' | tr -d '\n' | xclip -selection clipboard && xdotool key ctrl+v
