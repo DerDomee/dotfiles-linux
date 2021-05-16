d76041c93b33cc4f2a76e0e454f9df11328c114b
Dominik Riedig <me@dominikriedig.de>
2021-05-16 19:17:20 +0200

    Do not send notification on dunst start

--- .config/dunst/launch_dunst.sh
+++ .config/dunst/launch_dunst.sh
@@ -6,6 +6,5 @@ killall -q dunst
 while pgrep -u $UID -x dunst >/dev/null; do sleep 1; done
 # Start new polybar instances
 dunst -config "$HOME/.config/dunst/config" &
-notify-send -u low "Dunst launched" "Notification manager active"
 echo "Dunst launched"
 
