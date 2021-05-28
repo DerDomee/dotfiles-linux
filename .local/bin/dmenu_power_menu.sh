774391da864c023825f508fecd27e0ab7a2e9976
Dominik Riedig <me@dominikriedig.de>
2021-05-28 11:09:33 +0200

    Update dmenu_power_menu.sh

--- .local/bin/dmenu_power_menu.sh
+++ .local/bin/dmenu_power_menu.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-case $(echo -e "shutdown\nreboot\nlogout" | dmenu -c -l 3) in
+case $(echo -e "shutdown\nreboot\nlogout" | dmenu -c -r -F -l 3 -p "What to do?!") in
 	"shutdown") shutdown now;;
 	"reboot") reboot now;;
 	"logout") i3-msg exit;;
-	*) echo "ERROR!";;
+	*) exit;;
 esac
