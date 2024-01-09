10615b48a27b8e8e74b8f098ef4ac45e2f187642
Dominik Riedig <me@dominikriedig.de>
2024-01-09 22:58:11 +0100

    add nvim bufferline initial configuration

--- .config/nvim/lua/derdomee/config/bufferline.lua
+++ .config/nvim/lua/derdomee/config/bufferline.lua
@@ -1,2 +1,8 @@
 vim.opt.termguicolors = true
-require("bufferline").setup{}
+require("bufferline").setup{
+	options = {
+		separator_style = "slant",
+		show_buffer_icons = true,
+		persist_buffer_sort = true,
+	}
+}
